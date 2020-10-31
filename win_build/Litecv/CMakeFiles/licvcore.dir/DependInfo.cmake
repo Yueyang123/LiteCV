fbcf14239b4bc5509ff34154aaee6e518e90e333
Yueyang <1700695611@qq.com>
2020-11-01 00:15:52 +0800

    first commit

---
+++ win_build/Litecv/CMakeFiles/licvcore.dir/DependInfo.cmake
@@ -0,0 +1,24 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "E:/WORKPLACE/Litecv/LiteCV/Litecv/Core/bmp.c" "E:/WORKPLACE/Litecv/LiteCV/win_build/Litecv/CMakeFiles/licvcore.dir/Core/bmp.c.obj"
+  "E:/WORKPLACE/Litecv/LiteCV/Litecv/Core/li_image.c" "E:/WORKPLACE/Litecv/LiteCV/win_build/Litecv/CMakeFiles/licvcore.dir/Core/li_image.c.obj"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "../Litecv/inc/Core_inc"
+  "../Litecv/3rd/win_3rd_lib/jpeg_win_install/include"
+  "../Litecv/3rd/win_3rd_lib/png_win_install/include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
